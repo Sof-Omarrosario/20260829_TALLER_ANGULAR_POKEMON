1cfc6150129df5f63106c2dceb8d296744d32845
Sof-Omarrosario <omarramos1069@gmail.com>
2026-09-10 02:04:30 +0000

    Creació´n de Directivas y Servicios

--- buscadorPokemon/src/app/services/pokemon.storage.service.ts
+++ buscadorPokemon/src/app/services/pokemon.storage.service.ts
@@ -16,7 +16,7 @@ export interface PokemonTarjeta {
 })
 export class PokemonStorageService {
   private http = inject(HttpClient);
-  private readonly storageKey = 'equipo_pokemon_registrado';
+  private readonly STORAGE_KEY = 'equipo_pokemon_registrado';
 
   misPokemons = signal<PokemonTarjeta[]>([]);
 
@@ -25,9 +25,54 @@ export class PokemonStorageService {
   }
 
   private cargarPokemonsDesdeStorage() {
-    const data = localStorage.getItem(this.storageKey);
+    const data = localStorage.getItem(this.STORAGE_KEY);
     if (data) {
       this.misPokemons.set(JSON.parse(data));
     }
   }
-}
+
+
+
+  //-1. Obtener datos de la API
+
+  buscarEnApi(nombreOId: string) {
+    return this.http.get<any>('https://pokeapi.co/api/v2/pokemon/${nombreOId.toLowerCase}')
+
+  }
+
+
+
+  //-2. Guardar(Crear nuevo Pokemon dentro de el maleto.
+
+  guradarPokemon(nuevo: PokemonTarjeta) {
+      const actualizados = [... this.misPokemons(), nuevo];
+      this.misPokemons.set(actualizados); //Esta es la variabel Actual
+      localStorage.setItem(this.STORAGE_KEY, JSON.stringify(actualizados)) //Esta es la base de Datos
+
+  };
+
+
+
+  //-3. Actualizar Pokemon Favorito.
+
+  actualizarFavortio(id: number){
+    const actualizados = this.misPokemons().map(poke => {
+      if(poke.id === id) {
+        return { ...poke, esFavorito: !poke.esFavorito}
+      }
+      return poke;
+    });
+    this.misPokemons.set(actualizados);
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(actualizados));
+  };
+
+
+
+  //-4. Eliminar Pokemon del maleto.
+
+  eliminarPokemon(id: number) {
+    const filtrados = this.misPokemons().filter(poke => poke.id !== id);
+    this.misPokemons.set(filtrados);
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(filtrados));
+  };
+};
